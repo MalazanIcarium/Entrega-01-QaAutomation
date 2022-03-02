3c1872bf95dbbe3dc033a3e6b7e5a3b52d6438fd
Emmanuel Nol <42006907+MalazanIcarium@users.noreply.github.com>
2022-03-02 01:58:30 -0300

    Add files via upload

---
+++ src/test/java/testDemoblaze.java
@@ -0,0 +1,30 @@
+import Utility.DriverFactory;
+import Utility.PropertiesFile;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.sql.Driver;
+
+public class testDemoblaze {
+    private String url = PropertiesFile.getProperty("url");
+    public WebDriver driver = DriverFactory.getDriver();
+
+    @Test
+    public void testDemoblaze() {
+        driver.get(url);
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+
+        WebElement laptopsCategory = driver.findElement(By.linkText("Laptops"));
+        laptopsCategory.click();
+        WebElement laptopSonyVaioI5 = driver.findElement(By.linkText("Sony vaio i5"));
+        laptopSonyVaioI5.click();
+        /*WebElement laptopPrice = driver.findElement(By.className("price-container"));*/
+        
+    }
+
+}
